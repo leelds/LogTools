f4abb776d0f57aab9dec8d9c95723d53676b269c
ali <ali@secusoft.cc>
2019-04-10 10:01:45 +0800

    增加日志数据处理项目，基本UI已经完成

---
+++ LogDataLib/LogMoudle.fs
@@ -0,0 +1,61 @@
+﻿module LogMoudle
+open System.IO
+open System.Data
+open System.Text.RegularExpressions
+
+type ActionMode = Send|Recv|Null
+type MsgItem = {Time : string;CommandID :string;JsonMsg:string;ServerID:string; Actionmode:ActionMode;}
+   
+let mtime = @"\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}:\d{2}"
+let mSendAction = @"SendCmd"
+let mrecvAction = @"Call\sBack"
+let mserverID = @"\d{3}\.\d{3}\.\d{1,}\.\d{1,}\s\d{4,}"
+let mCommandID = @"CommandID\,\d{4,}"
+let mcmdType = @"cmdType\sis\s\:\s{1,}\d{4,}"
+let mjson = @"{[\S\s]+}"
+
+let mathcSometing str matc =
+    let re = Regex.Match(str,matc)
+    if re.Success then re.Value else "null"
+
+let getItem str =
+    let commandID = mathcSometing str mCommandID
+                   |> (fun str->str.Split(','))
+                   |> (fun strs->if strs.Length <> 1 then strs.[1] else strs.[0])
+
+    let cmdType =  mathcSometing str mcmdType
+                   |>(fun str->str.Split(':'))
+                   |>(fun strs->if strs.Length <> 1 then strs.[1] else strs.[0])
+
+    let CommandId = match commandID,cmdType with
+                    | c,"null" -> c
+                    |"null",d -> d
+                    |_ ->"null"
+
+    let act=match (mathcSometing str mSendAction,mathcSometing str mrecvAction) with
+            |"null","null"->ActionMode.Null
+            |_,"null" -> ActionMode.Send
+            |"null",_ -> ActionMode.Recv
+            |_ ->ActionMode.Null
+    {
+       Time = mathcSometing str mtime;
+       CommandID = CommandId;
+       JsonMsg = mathcSometing str mjson; 
+       ServerID = mathcSometing str mserverID;
+       Actionmode = act;
+    }
+
+let getMsgItems path=
+    File.ReadAllText(path)
+    |>Regex("\[Info\]:").Split
+    |>Seq.map (fun info->getItem info)
+
+let getTbale items  =
+    let table=new DataTable()
+    ["Time";"CommandID";"Actionmode";"JsonMsg";"ServerID";]|>Seq.iter (fun name-> table.Columns.Add(name)|>ignore)
+    items|>Seq.iter (fun row->table.Rows.Add(row.Time,row.CommandID,row.Actionmode,row.JsonMsg,row.ServerID)|>ignore)
+    table
+
+let GetTableByPath path = 
+    path|> getMsgItems |> getTbale
+
--- LogToolsUI/MainWindow.xaml.cs
+++ LogToolsUI/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,19 +21,55 @@ namespace LogToolsUI
     /// </summary>
     public partial class MainWindow : Window
     {
+        class T1
+        {
+            public string PathStr { get; set; }
+            public T1(string path)
+            {
+                PathStr = path;
+            }
+        }
         public MainWindow()
         {
             InitializeComponent();
+            Loaded += (s, e) =>
+            {
+                dataGrid.DataContext = makeTable();
+                listBox.ItemsSource = new List<T1>() { new T1("1"), new T1("t2") };
+            };
         }
-
-        private void ColumnDefinition_MouseWheel(object sender, MouseWheelEventArgs e)
+        DataTable makeTable()
         {
-
+            var ta = new DataTable();
+            new List<string>() { "CommandID", "ActionMode", "ServerID", "Time" }.ForEach(header => ta.Columns.Add(header));
+            new List<string>() { "40005", "20", "3" }.ForEach(row => ta.Rows.Add(row, "10", "a", "c"));
+            return ta;
         }
 
         private void Txt_TextChanged(object sender, TextChangedEventArgs e)
         {
 
+        }
+       
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var txtStr = txt.Text;
+            var a1 = txtStr.Split('&');
+            foreach (var item in a1)
+            {
+
+            }
+        }
+
+        DataTable SelectTable(DataTable table,string SelectStr)
+        {
+            var ctable = table.Clone();
+            var rows = table.Select(SelectStr);
+            foreach (var row in rows)
+            {
+                ctable.Rows.Add(row.ItemArray);
+            }
+            return ctable;
         }
     }
 }
